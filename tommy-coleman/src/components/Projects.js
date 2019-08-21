2c4be1e29706668ee76e86b289667efca21d67b9
Tommy Coleman <tommiilee@gmail.com>
2019-08-21 15:53:53 -0400

    added harry potter project to projects tab

--- tommy-coleman/src/components/Projects.js
+++ tommy-coleman/src/components/Projects.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import hpprojectthumbnail from '../assets/hpprojectthumbnail.jpg'
+import {ProjectImg} from '../styles/projectStyles';
 const Projects = () => {
     return (
         <div>
             <h1>Projects</h1>
+            <div>
+                <h2>Harry Potter House Points Project</h2>
+                <a href='https://react-redux-app-g4ch05rym.now.sh/' target='_blank'><ProjectImg src={hpprojectthumbnail} /></a>
+            </div>
         </div>
     )
 }
---
+++ tommy-coleman/src/styles/projectStyles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ProjectImg = styled.img`
+width: 100px;
+height: 100px;`
