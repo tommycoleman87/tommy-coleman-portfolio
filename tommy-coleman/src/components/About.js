41342ac0b3d4ed04826ff150f7d1345e11f2ab7a
Tommy Coleman <tommiilee@gmail.com>
2019-08-21 15:21:36 -0400

    styled about with flexbox

--- tommy-coleman/src/components/About.js
+++ tommy-coleman/src/components/About.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import tommycolemanprofile from '../assets/tommycolemanprofile.jpg';
-import { ProfileImg } from '../styles/tabStyles'
+import { ProfileImg, AboutDiv } from '../styles/tabStyles'
 const About = () => {
     return (
-        <div>
+        <AboutDiv>
             <div><ProfileImg src={tommycolemanprofile} alt='Tommy Coleman Profile' /></div>
             <div><h1>Hello, I'm Tommy</h1>
             <p>I am a full stack web developer in Tampa, Fl. Versed in the MERN stack with a love of Javascript and React. </p></div>
-        </div>
+        </AboutDiv>
 
     )
 }
--- tommy-coleman/src/styles/tabStyles.js
+++ tommy-coleman/src/styles/tabStyles.js
@@ -8,3 +8,8 @@ max-width: 1000px;
 export const ProfileImg = styled.img`
 width: 200px;
 height: 200px;`
+
+export const AboutDiv = styled.div`
+display: flex;
+justify-content: space-around;
+`
